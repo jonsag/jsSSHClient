9b1cf9c315b8efc60760609c617f06a0af09b071
jonsag <jonsagebrand@gmail.com>
2014-08-30 22:18:22 +0200

    More work on adding hosts

--- myFunctions.py
+++ myFunctions.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 # Encoding: UTF-8
 
-import sys, ConfigParser, os, getpass
+import sys, ConfigParser, os, getpass, subprocess
 
 # starting up some variables
 
 config = ConfigParser.ConfigParser()
 config.read("%s/config.ini" % os.path.dirname(__file__)) # read config file
+
+hosts = ConfigParser.ConfigParser()
+hosts.read("%s/myHosts.ini" % os.path.dirname(__file__)) # read config file
 
 clientType = (config.get('ssh','clientType'))
 
@@ -34,12 +37,34 @@ def usage(exitCode):
 
 def newHost(verbose):
     print "--- Add host"
-    hostName = raw_input("Host name: ")
-    hostAddress = raw_input("Address:")
+    existingHosts = hosts.sections()
+    hostExist = False
+    while True:
+        hostAddress = raw_input("Address: ") # input host name or IP
+        if pingHost(hostAddress): # check if we can ping host
+            hostIsUp = True
+        else:
+            print "Could not ping %s" % hostAddress
+            hostIsUp = False
+            
+        userName = raw_input("User name: ") # input user to log in with
+        
+        for host in existingHosts: # check if that combination already exist in host file
+            if "%s_%s" % (hostAddress, userName) == host:
+                print "%s with user %s already in list. Try again" % (hostAddress, userName)
+                hostAccepted = False
+                break
+            else:
+                hostAccepted = True
+                
+        if hostAccepted and hostIsUp:
+            print "--- Everything OK"
+            break
+    
     hostPort = raw_input("Port (22): ")
     if not hostPort:
         hostPort = 22
-    userName = raw_input("User name: ")
+    
     while True:
         passWord = getpass.getpass("Password: ")
         passWord2 = getpass.getpass("Password again: ")
@@ -47,7 +72,28 @@ def newHost(verbose):
             break
         else:
             print "*** Passwords don't match. Try again"
-    configEntry = "%s-%s" % (hostName, userName)
-    hostParams = (hostName, hostAddress, hostPort, userName, passWord)
+
+    hostEntry = "%s_%s" % (hostAddress, userName)
+    
+    hostParams = (hostAddress, hostPort, userName, passWord)
     print hostParams
-    print "--- Adding %s to your hosts" % configEntry
+    
+    print "--- Adding %s to your hosts" % hostEntry   
+    hostsFile = open("%s/myHosts.ini" % os.path.dirname(__file__),'w') # write to hosts file
+    hosts.add_section(hostEntry)
+    hosts.set(hostEntry,'hostAddress', hostAddress)
+    hosts.set(hostEntry,'hostPort', hostPort)
+    hosts.set(hostEntry,'userName', userName)
+    hosts.set(hostEntry,'passWord', passWord)
+    hosts.write(hostsFile)
+    hostsFile.close()
+    
+def pingHost(host):
+    pingResponse = subprocess.Popen(["ping", "-c1", "-w100", host], stdout=subprocess.PIPE).stdout.read()
+    if not pingResponse:
+        print "*** %s is down" % host
+        result = False
+    else:
+        print "--- %s is up" % host
+        result = True
+    return result
